5258cbee0bb4f9e62106f563ed8b4157866a0779
grvmauli <grvmauli@gmail.com>
2026-09-05 15:37:59 +0100

    chore: establish project baseline

---
+++ vision_test.py
@@ -0,0 +1,29 @@
+import cv2
+
+import torch
+
+
+
+print("=== Vision Workstation Test ===")
+
+print("OpenCV version:", cv2.__version__)
+
+print("PyTorch version:", torch.__version__)
+
+print("CUDA available:", torch.cuda.is_available())
+
+
+
+image = cv2.imread("test.jpg")
+
+
+
+if image is None:
+
+	print("Image test: waiting for test.jpg")
+
+else:
+
+	print("Image test: SUCCESS")
+
+print("Image dimensions:", image.shape)
